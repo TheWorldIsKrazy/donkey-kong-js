edeea094bdfa58bcb2f0873c9691d74b30d82ece
Etienne DELADONCHAMPS <etienne.ladonch@outlook.com>
2015-02-04 12:30:53 +0100

    Copy from monster

---
+++ js/app.js
@@ -0,0 +1,45 @@
+var canvas = document.getElementById('monster');
+var ctx = canvas.getContext('2d');
+
+// Events
+canvas.addEventListener('click', function(event) {
+	if (monster.currentAnimation == 'jump') {
+		monster.setAnimation('boom');
+		window.setTimeout(function() {
+			monster.setAnimation('jump');
+		}, 2000);
+	}
+});
+
+// Monster Sprite
+var monster = new Sprite({
+	source : 'images/monsterTileSheet.png',
+	size : {height: 128, width: 128},
+	rows : 3,
+	nbrOfImages: 7,
+	canvas: canvas,
+	animations: {
+		'jump': {
+			min: 0,
+			max:5,
+			bounceMin: function(sprite) {
+				sprite.setAnimation('hide');
+				sprite.reverse = false;
+				delay = (Math.random()*2*1000);
+				window.setTimeout(function() {
+					sprite.setAnimation('jump');
+				}, delay);
+			}
+		},
+		'hide': {min: 0, max:0},
+		'boom': {min: 6, max:6}
+	},
+	currentAnimation: 'jump',
+});
+
+var render = function() {
+	monster.update();
+	window.setTimeout(render, 100);
+}
+
+render();
