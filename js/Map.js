4de1ad54c07d18bdda73a49da404c09405f352f7
Etienne DELADONCHAMPS <etienne.ladonch@outlook.com>
2015-02-07 16:08:11 +0100

    Affichage block ok

--- js/Map.js
+++ js/Map.js
@@ -1,5 +1,10 @@
 var Map = function(params) {
 	this.data = params.levels;
+	this.level = 0;
+	this.grid = params.grid;
+	this.size = params.size;
+	this.ctx = params.ctx;
+	this.onload = params.onload;
 	
 	// Images
 	that = this;
@@ -9,17 +14,32 @@ var Map = function(params) {
 		echelle: new Image(),
 	};
 	this.images.block.addEventListener("load", function() {
-		console.log("img1");
 		that.imgLoaded();
 	}, false);
 	this.images.echelle.addEventListener("load", function() {
-		console.log("img2");
 		that.imgLoaded();
 	}, false);
-	this.images.block.scr = 'images/block.png';
-	this.images.echelle.scr = 'images/echelle.png';
+	this.images.block.src = 'images/block.png';
+	this.images.echelle.src = 'images/echelle.png';
 
-	this.init();
+	// Set canvas Size
+	this.ctx.canvas.height = this.size.height * this.grid.height;
+	this.ctx.canvas.width = this.size.width * this.grid.width;
+
+	// data validation
+	for (var i = 0; i < this.data.length; i++) {
+		lvl = this.data[i];
+		for (var grid in lvl) {
+			if (lvl[grid].length != this.size.height) {
+				console.log("La hauteur de " + grid + " du niveau " + i + " ne correspond pas !");
+			}
+			for (var j = 0; j < lvl[grid].length; j++) {
+				if (lvl[grid][j].length != this.size.width) {
+					console.log("La largeur de " + grid + " du niveau " + i + " ne correspond pas !");
+				}
+			};
+		}
+	}
 };
 
 Map.prototype.init = function() {
@@ -27,7 +47,25 @@ Map.prototype.init = function() {
 };
 
 Map.prototype.display = function() {
-	
+	lvl = this.getCurrentLevel();
+	for (var i = 0; i < this.data.length; i++) {
+		lvl = this.data[i];
+		for (var j = 0; j < lvl["blocks"].length; j++) {
+			for (var k = 0; k < lvl["blocks"][j].length; k++) {
+				//lvl["blocks"][j][k];
+				if (lvl["blocks"][j][k] != 9) {
+					this.ctx.drawImage(this.images.block,
+						this.grid.width*k, this.grid.height*j + lvl["blocks"][j][k],
+						this.grid.width, this.grid.height
+					);
+				}
+			}
+		}
+	}
+};
+
+Map.prototype.getCurrentLevel = function() {
+	return this.data[this.level];
 };
 
 Map.prototype.imgLoaded = function() {
@@ -37,6 +75,7 @@ Map.prototype.imgLoaded = function() {
 		nbrOfImages++;
 	};
 	if (nbrOfImages == this.imgsOK) {
-		console.log("loaded !");
+		console.log("All resources are loaded !");
+		this.onload(this);
 	}
 };
--- js/app.js
+++ js/app.js
@@ -3,6 +3,12 @@ var ctx = canvas.getContext('2d');
 
 map = new Map({
 	levels: levels,
+	size: {width: 28, height:28},
+	grid: {width: 8, height: 8},
+	ctx: ctx,
+	onload: function(map) {
+		map.display();
+	}
 });
 
 var render = function() {
