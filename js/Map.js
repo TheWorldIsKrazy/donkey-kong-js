b11de5c1d3a85171c0c0f6f888067260aa9f28e1
Antoine Portejoie <antoine.portejoie@outlook.fr>
2015-02-12 23:13:50 +0100

    Ajout d'un niveau entier
    La manière est un peu brute et mérite certainement un petit
    réarrangement.

--- js/Map.js
+++ js/Map.js
@@ -1,6 +1,6 @@
 var Map = function(params) {
 	this.data = params.levels;
-	this.level = 0;
+	this.level = 1;
 	this.grid = params.grid;
 	this.size = params.size;
 	this.ctx = params.ctx;
@@ -10,17 +10,38 @@ var Map = function(params) {
 	that = this;
 	this.imgsOK = 0;
 	this.images = {
-		block: new Image(),
-		echelle: new Image(),
+		blockRouge: new Image(),
+		blockJaune: new Image(),
+		blockCarreJaune: new Image(),
+		croix: new Image(),
+		echelleBleu: new Image(),
+		echelleBlanche: new Image(),
 	};
-	this.images.block.addEventListener("load", function() {
+	this.images.blockRouge.addEventListener("load", function() {
 		that.imgLoaded();
 	}, false);
-	this.images.echelle.addEventListener("load", function() {
+	this.images.blockJaune.addEventListener("load", function() {
 		that.imgLoaded();
 	}, false);
-	this.images.block.src = 'images/block.png';
-	this.images.echelle.src = 'images/echelle.png';
+	this.images.blockCarreJaune.addEventListener("load", function() {
+		that.imgLoaded();
+	}, false);
+	this.images.croix.addEventListener("load", function() {
+		that.imgLoaded();
+	}, false);
+	this.images.echelleBleu.addEventListener("load", function() {
+		that.imgLoaded();
+	}, false);
+	this.images.echelleBlanche.addEventListener("load", function() {
+		that.imgLoaded();
+	}, false);
+
+	this.images.blockRouge.src = 'images/blockRouge.png';
+	this.images.blockJaune.src = 'images/blockJaune.png';
+	this.images.blockCarreJaune.src = 'images/blockCarreJaune.png';
+	this.images.croix.src = 'images/croix.png';
+	this.images.echelleBleu.src = 'images/echelleBleu.png';
+	this.images.echelleBlanche.src = 'images/echelleBlanche.png';
 
 	// Set canvas Size
 	this.ctx.canvas.height = this.size.height * this.grid.height;
@@ -54,23 +75,61 @@ Map.prototype.display = function() {
 	this.ctx.fill();
 
 	lvl = this.getCurrentLevel();
-	for (var j = 0; j < lvl["echelles"].length; j++) {
-		for (var k = 0; k < lvl["echelles"][j].length; k++) {
-			//lvl["blocks"][j][k];
-			if (lvl["echelles"][j][k] == 1) {
-				this.ctx.drawImage(this.images.echelle,
-					this.grid.width*k, this.grid.height*j,
-					this.grid.width, this.grid.height
-				);
+	if (this.level == 0) {
+		for (var j = 0; j < lvl["echelles"].length; j++) {
+			for (var k = 0; k < lvl["echelles"][j].length; k++) {
+				//lvl["blocks"][j][k];
+				if (lvl["echelles"][j][k] == 1) {
+					this.ctx.drawImage(this.images.echelleBleu,
+						this.grid.width*k, this.grid.height*j,
+						this.grid.width, this.grid.height
+					);
+				}
+			}
+		}
+		for (var j = 0; j < lvl["blocks"].length; j++) {
+			for (var k = 0; k < lvl["blocks"][j].length; k++) {
+				//lvl["blocks"][j][k];
+				if (lvl["blocks"][j][k] != 9) {
+					this.ctx.drawImage(this.images.blockRouge,
+						this.grid.width*k, this.grid.height*j + lvl["blocks"][j][k],
+						this.grid.width, this.grid.height
+					);
+				}
+			}
+		}
+	}
+	if (this.level == 1) {
+		for (var j = 0; j < lvl["echelles"].length; j++) {
+			for (var k = 0; k < lvl["echelles"][j].length; k++) {
+				//lvl["blocks"][j][k];
+				if (lvl["echelles"][j][k] == 1) {
+					this.ctx.drawImage(this.images.echelleBlanche,
+						this.grid.width*k, this.grid.height*j,
+						this.grid.width, this.grid.height
+					);
+				}
+				if (lvl["echelles"][j][k] == 2) {
+					this.ctx.drawImage(this.images.croix,
+						this.grid.width*k, this.grid.height*j,
+						this.grid.width, this.grid.height
+					);
+				}
 			}
 		}
 	}
 	for (var j = 0; j < lvl["blocks"].length; j++) {
 		for (var k = 0; k < lvl["blocks"][j].length; k++) {
 			//lvl["blocks"][j][k];
-			if (lvl["blocks"][j][k] != 9) {
-				this.ctx.drawImage(this.images.block,
-					this.grid.width*k, this.grid.height*j + lvl["blocks"][j][k],
+			if (lvl["blocks"][j][k] == 0) {
+				this.ctx.drawImage(this.images.blockJaune,
+					this.grid.width*k, this.grid.height*j,
+					this.grid.width, this.grid.height
+				);
+			}
+			if (lvl["blocks"][j][k] == 1) {
+				this.ctx.drawImage(this.images.blockCarreJaune,
+					this.grid.width*k, this.grid.height*j,
 					this.grid.width, this.grid.height
 				);
 			}
