e60d9b049416c2f84a8d3f3872c077a7cc98a5aa
Etienne DELADONCHAMPS <etienne.ladonch@outlook.com>
2015-02-13 14:49:26 +0100

    ??

--- js/Map.js
+++ js/Map.js
@@ -4,44 +4,40 @@ var Map = function(params) {
 	this.grid = params.grid;
 	this.size = params.size;
 	this.ctx = params.ctx;
-	this.onload = params.onload;
-	
-	// Images
-	that = this;
-	this.imgsOK = 0;
-	this.images = {
-		blockRouge: new Image(),
-		blockJaune: new Image(),
-		blockCarreJaune: new Image(),
-		croix: new Image(),
-		echelleBleu: new Image(),
-		echelleBlanche: new Image(),
-	};
-	this.images.blockRouge.addEventListener("load", function() {
-		that.imgLoaded();
-	}, false);
-	this.images.blockJaune.addEventListener("load", function() {
-		that.imgLoaded();
-	}, false);
-	this.images.blockCarreJaune.addEventListener("load", function() {
-		that.imgLoaded();
-	}, false);
-	this.images.croix.addEventListener("load", function() {
-		that.imgLoaded();
-	}, false);
-	this.images.echelleBleu.addEventListener("load", function() {
-		that.imgLoaded();
-	}, false);
-	this.images.echelleBlanche.addEventListener("load", function() {
-		that.imgLoaded();
-	}, false);
 
-	this.images.blockRouge.src = 'images/blockRouge.png';
-	this.images.blockJaune.src = 'images/blockJaune.png';
-	this.images.blockCarreJaune.src = 'images/blockCarreJaune.png';
-	this.images.croix.src = 'images/croix.png';
-	this.images.echelleBleu.src = 'images/echelleBleu.png';
-	this.images.echelleBlanche.src = 'images/echelleBlanche.png';
+	// this.images = {
+	// 	blockRouge: new Image(),
+	// 	blockJaune: new Image(),
+	// 	blockCarreJaune: new Image(),
+	// 	croix: new Image(),
+	// 	echelleBleu: new Image(),
+	// 	echelleBlanche: new Image(),
+	// };
+	// this.images.blockRouge.addEventListener("load", function() {
+	// 	that.imgLoaded();
+	// }, false);
+	// this.images.blockJaune.addEventListener("load", function() {
+	// 	that.imgLoaded();
+	// }, false);
+	// this.images.blockCarreJaune.addEventListener("load", function() {
+	// 	that.imgLoaded();
+	// }, false);
+	// this.images.croix.addEventListener("load", function() {
+	// 	that.imgLoaded();
+	// }, false);
+	// this.images.echelleBleu.addEventListener("load", function() {
+	// 	that.imgLoaded();
+	// }, false);
+	// this.images.echelleBlanche.addEventListener("load", function() {
+	// 	that.imgLoaded();
+	// }, false);
+
+	// this.images.blockRouge.src = 'images/blockRouge.png';
+	// this.images.blockJaune.src = 'images/blockJaune.png';
+	// this.images.blockCarreJaune.src = 'images/blockCarreJaune.png';
+	// this.images.croix.src = 'images/croix.png';
+	// this.images.echelleBleu.src = 'images/echelleBleu.png';
+	// this.images.echelleBlanche.src = 'images/echelleBlanche.png';
 
 	// Set canvas Size
 	this.ctx.canvas.height = this.size.height * this.grid.height;
@@ -76,66 +72,74 @@ Map.prototype.display = function() {
 
 	lvl = this.getCurrentLevel();
 
-	if (this.level == 0) {
-		for (var j = 0; j < lvl["echelles"].length; j++) {
-			for (var k = 0; k < lvl["echelles"][j].length; k++) {
-				//lvl["blocks"][j][k];
-				if (lvl["echelles"][j][k] == 1) {
-					this.ctx.drawImage(this.images.echelleBleu,
-						this.grid.width*k, this.grid.height*j,
-						this.grid.width, this.grid.height
-					);
-				}
-			}
-		}
-		for (var j = 0; j < lvl["blocks"].length; j++) {
-			for (var k = 0; k < lvl["blocks"][j].length; k++) {
-				//lvl["blocks"][j][k];
-				if (lvl["blocks"][j][k] != 9) {
-					this.ctx.drawImage(this.images.blockRouge,
-						this.grid.width*k, this.grid.height*j + lvl["blocks"][j][k],
-						this.grid.width, this.grid.height
-					);
-				}
-			}
-		}
-	}
-	if (this.level == 1) {
-		for (var j = 0; j < lvl["echelles"].length; j++) {
-			for (var k = 0; k < lvl["echelles"][j].length; k++) {
-				//lvl["blocks"][j][k];
-				if (lvl["echelles"][j][k] == 1) {
-					this.ctx.drawImage(this.images.echelleBlanche,
-						this.grid.width*k, this.grid.height*j,
-						this.grid.width, this.grid.height
-					);
-				}
-				if (lvl["echelles"][j][k] == 2) {
-					this.ctx.drawImage(this.images.croix,
-						this.grid.width*k, this.grid.height*j,
-						this.grid.width, this.grid.height
-					);
-				}
-			}
-		}
-	}
-	for (var j = 0; j < lvl["blocks"].length; j++) {
-		for (var k = 0; k < lvl["blocks"][j].length; k++) {
-			//lvl["blocks"][j][k];
-			if (lvl["blocks"][j][k] == 0) {
-				this.ctx.drawImage(this.images.blockJaune,
-					this.grid.width*k, this.grid.height*j,
-					this.grid.width, this.grid.height
-				);
-			}
-			if (lvl["blocks"][j][k] == 1) {
-				this.ctx.drawImage(this.images.blockCarreJaune,
-					this.grid.width*k, this.grid.height*j,
-					this.grid.width, this.grid.height
-				);
-			}
-		}
-	}
+	// Load level's images
+	this.images = {};
+	this.imgsOK = 0;
+
+	for (var name in lvl.images) {
+		this.images[lvl.images[i]]
+	};
+
+	// if (this.level == 0) {
+	// 	for (var j = 0; j < lvl["echelles"].length; j++) {
+	// 		for (var k = 0; k < lvl["echelles"][j].length; k++) {
+	// 			//lvl["blocks"][j][k];
+	// 			if (lvl["echelles"][j][k] == 1) {
+	// 				this.ctx.drawImage(this.images.echelleBleu,
+	// 					this.grid.width*k, this.grid.height*j,
+	// 					this.grid.width, this.grid.height
+	// 				);
+	// 			}
+	// 		}
+	// 	}
+	// 	for (var j = 0; j < lvl["blocks"].length; j++) {
+	// 		for (var k = 0; k < lvl["blocks"][j].length; k++) {
+	// 			//lvl["blocks"][j][k];
+	// 			if (lvl["blocks"][j][k] != 9) {
+	// 				this.ctx.drawImage(this.images.blockRouge,
+	// 					this.grid.width*k, this.grid.height*j + lvl["blocks"][j][k],
+	// 					this.grid.width, this.grid.height
+	// 				);
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// if (this.level == 1) {
+	// 	for (var j = 0; j < lvl["echelles"].length; j++) {
+	// 		for (var k = 0; k < lvl["echelles"][j].length; k++) {
+	// 			//lvl["blocks"][j][k];
+	// 			if (lvl["echelles"][j][k] == 1) {
+	// 				this.ctx.drawImage(this.images.echelleBlanche,
+	// 					this.grid.width*k, this.grid.height*j,
+	// 					this.grid.width, this.grid.height
+	// 				);
+	// 			}
+	// 			if (lvl["echelles"][j][k] == 2) {
+	// 				this.ctx.drawImage(this.images.croix,
+	// 					this.grid.width*k, this.grid.height*j,
+	// 					this.grid.width, this.grid.height
+	// 				);
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// for (var j = 0; j < lvl["blocks"].length; j++) {
+	// 	for (var k = 0; k < lvl["blocks"][j].length; k++) {
+	// 		//lvl["blocks"][j][k];
+	// 		if (lvl["blocks"][j][k] == 0) {
+	// 			this.ctx.drawImage(this.images.blockJaune,
+	// 				this.grid.width*k, this.grid.height*j,
+	// 				this.grid.width, this.grid.height
+	// 			);
+	// 		}
+	// 		if (lvl["blocks"][j][k] == 1) {
+	// 			this.ctx.drawImage(this.images.blockCarreJaune,
+	// 				this.grid.width*k, this.grid.height*j,
+	// 				this.grid.width, this.grid.height
+	// 			);
+	// 		}
+	// 	}
+	// }
 };
 
 Map.prototype.getCurrentLevel = function() {
@@ -150,6 +154,5 @@ Map.prototype.imgLoaded = function() {
 	};
 	if (nbrOfImages == this.imgsOK) {
 		console.log("All resources are loaded !");
-		this.onload(this);
 	}
 };
--- js/app.js
+++ js/app.js
@@ -6,9 +6,6 @@ map = new Map({
 	size: {width: 28, height:28},
 	grid: {width: 8, height: 8},
 	ctx: ctx,
-	onload: function(map) {
-		map.display();
-	}
 });
 
 var render = function() {
