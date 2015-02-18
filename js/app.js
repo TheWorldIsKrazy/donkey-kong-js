beacbba5fc0cd927d3e6105a5c8f8a4640bd5011
Etienne DELADONCHAMPS <etienne.ladonch@outlook.com>
2015-02-18 09:16:27 +0100

    grid size up : 16

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var ctx = canvas.getContext('2d');
 map = new Map({
 	levels: levels,
 	size: {width: 28, height:28},
-	grid: {width: 8, height: 8},
+	grid: {width: 16, height: 16},
 	ctx: ctx,
 });
 
