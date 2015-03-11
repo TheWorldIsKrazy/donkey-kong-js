a2d7c455140cf7e5f2a6b65d12c674912230258d
Antoine Portejoie <antoine.portejoie@outlook.fr>
2015-03-11 11:16:30 +0100

    Class Layer mieu
    Layer.proto.clear

--- js/Layer.js
+++ js/Layer.js
@@ -3,7 +3,7 @@ var Layer = function (zIndex, size) {
 	for (var i = 0; i < Layer.all.length; i++) {
 		lay = Layer.all[i];
 		if (lay.zIndex == zIndex) {
-			console.log("zIndex "+zIndex+" déjà pris");
+			console.log("Le zIndex "+zIndex+" déjà pris");
 			return	false;
 		}
 	}
@@ -14,12 +14,17 @@ var Layer = function (zIndex, size) {
 	this.ctx = this.canvas.getContext('2d');
 	document.getElementsByTagName('body')[0].appendChild(this.canvas);
 	this.zIndex = zIndex;
+	this.size = size;
 	this.ctx.canvas.height = size.height;
 	this.ctx.canvas.width = size.width;
 
 	Layer.all.push(this);
 	return this;
 
+};
+
+Layer.prototype.clear = function() {
+	this.ctx.clearRect(0,0,this.size.height,this.size.width);
 };
 
 Layer.all = new Array();
